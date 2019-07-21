3348f10b13118784ebfc546a53baf1cd0d3e2fa7
maestro <dream.big.dev.423@gmail.com>
2019-07-21 18:20:36 +0300

    Add layout and sidebar

---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout">
+    <Sidebar />
+    <transition name="router-animation">
+      <router-view />
+    </transition>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar/Sidebar'
+
+export default {
+  name: 'Layout',
+  components: { Sidebar }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './Layout.scss';
+</style>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-navbar
+    :class="{ sidebar: true }"
+  >
+    <b-navbar-brand>
+      Immocloud
+    </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<style src="./Sidebar.scss" lang="scss" scoped>
