f90c93c7d78d24344dc269efb57f060cf09bdbce
maestro <dream.big.dev.423@gmail.com>
2019-07-22 23:20:57 +0300

    install font-awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
